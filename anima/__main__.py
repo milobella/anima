e94ab17a0c566c5b9c5f8e8097cbcb313fee471a
Célian Garcia <celian.garcia1@gmail.com>
2018-12-08 18:25:14 +0100

    Add some processing types to the NLG itnerface

--- anima/__main__.py
+++ anima/__main__.py
@@ -32,11 +32,35 @@ async def home(request):
     return response.html('<p>Hello world!</p>')
 
 
+def process_time(val):
+    return val
+
+
+def process_enumerated_list(val):
+    if len(val) == 1:
+        return val
+    start = ", ".join(val[:-1])
+    return "{0} {1} {2}".format(start, _mapping.get_sentence("and"), val[-1])
+
+
+def process_value(param):
+    if "type" not in param:
+        return param['value']
+    elif param["type"] == "time":
+        return process_time(param['value'])
+    elif param["type"] == "enumerated_list":
+        return process_enumerated_list(param['value'])
+
+
 @_app.route('/restitute', methods=["POST"])
 async def restitute(request: Request):
     translated_sentence = _mapping.get_sentence(request.json["sentence"])
-    params = request.json.get("params", {})
-    return response.text(Jinja2Resolver.resolve(translated_sentence, params))
+    params = request.json.get("params", [])
+    jinja_params = {}
+    for p in params:
+        jinja_params[p["name"]] = process_value(p)
+
+    return response.text(Jinja2Resolver.resolve(translated_sentence, jinja_params))
 
 
 if __name__ == '__main__':
